8f97c12d16d2d00343132886f4f79da663b6e254
mmoreland <epicswimlegend@yahoo.com>
2025-10-15 06:53:49 -0700

    added grid size button, to allow for changes to grid size by clearing and remaking

--- etch.js
+++ etch.js
@@ -1,28 +1,39 @@
-const cont = document.querySelector("div");
+const butt = document.querySelector("button");
+let gridSize = 16;
 
-for(let x=0; x < 16; x++){
-    newDiv = document.createElement("div");
-    cont.appendChild(newDiv);
-    
-    for(let y=0; y < 16; y++){
-        vertDiv = document.createElement("div");
-        vertDiv.className = "grid";
-        vertContent = document.createTextNode(y + 1);
-        let temp = vertDiv;
-        temp.addEventListener('mouseenter', () => {
-            temp.setAttribute("style", "background-color: black;");
-        });
-        newDiv.appendChild(vertDiv);
+butt.addEventListener("click", () =>{
+        let gridSize = prompt("Please enter new grid size between 1 and 100");
+        if(gridSize < 100 && gridSize > 0){
+            const elementsToRemove= document.querySelectorAll('.grid');
+            elementsToRemove.forEach(element => {
+                element.remove();
+            });
+            make_grid(gridSize);
+        }else{
+            gridSize = alert("Too large, Please enter new grid size between 1-100")
+        }
+});
+
+function make_grid(gSize){
+    const cont = document.querySelector("div");
+
+    for(let x=0; x < gSize; x++){
+        newDiv = document.createElement("div");
+        cont.appendChild(newDiv);
+        
+        for(let y=0; y < gSize; y++){
+            vertDiv = document.createElement("div");
+            vertDiv.className = "grid";
+            let temp = vertDiv; //temp allows each grid to retain the attribute instead of just the last made one
+            temp.addEventListener('mouseenter', () => {
+                temp.setAttribute("style", "background-color: black;");
+            });
+            newDiv.appendChild(vertDiv);
+        }
     }
 }
 
-var grids = document.getElementsByClassName("grid");
+    //var grids = document.getElementsByClassName("grid");
 
-/*for (var i = 0; i < grids.length; i++) {
-    var myTurn = grids[i];
-    grids[i].addEventListener('mouseenter', () => {
-        grids[i].setAttribute("style", "background-color: #ccc;");
-    });    
-}
-    */
-        
+make_grid(gridSize);
+
